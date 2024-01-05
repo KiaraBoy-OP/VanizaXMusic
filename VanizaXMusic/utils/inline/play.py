2421ce7946b0c2a348d9a7ca817d1d7eb32f699e
Zaunka StripeX <109966663+KiaraBoy-OP@users.noreply.github.com>
2024-01-05 08:55:51 +0530

    Update play.py

--- VanizaXMusic/utils/inline/play.py
+++ VanizaXMusic/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "✶—————————"
+        bar = "ᐈ—————————"
     elif 10 < umm < 20:
-        bar = "—✶————————"
+        bar = "—ᐈ————————"
     elif 20 <= umm < 30:
-        bar = "——✶———————"
+        bar = "——ᐈ———————"
     elif 30 <= umm < 40:
-        bar = "———✶——————"
+        bar = "———ᐈ——————"
     elif 40 <= umm < 50:
-        bar = "————✶—————"
+        bar = "————ᐈ—————"
     elif 50 <= umm < 60:
-        bar = "—————✶————"
+        bar = "—————ᐈ————"
     elif 60 <= umm < 70:
-        bar = "——————✶———"
+        bar = "——————ᐈ———"
     elif 70 <= umm < 80:
-        bar = "———————✶——"
+        bar = "———————ᐈ——"
     elif 80 <= umm < 95:
-        bar = "————————✶—"
+        bar = "————————ᐈ—"
     else:
-        bar = "—————————✶"
+        bar = "—————————ᐈ"
     buttons = [
          [
             InlineKeyboardButton(
